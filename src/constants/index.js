73e6a4def29b125bf740f77ed55e4bd54699f415
Vadym <vadymlysytskyi@gmail.com>
2024-11-10 19:26:57 +0100

    fixing issues with AWS check

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,8 @@ export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const __dirname = path.join(process.cwd(), "src");
-export const certsDir = path.join(process.cwd(), "src", "certificates");
+
+const isRender = process.env.RENDER === "true";
+export const certsDir = isRender
+  ? path.join(process.cwd(), "certificates")
+  : path.join(process.cwd(), "src", "certificates");
