a008aa5087f598b7d96efad4f563413f9d1c2993
Vadym <vadymlysytskyi@gmail.com>
2024-11-19 22:15:31 +0100

    Fixing mongodb issues on render

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,12 +3,24 @@ import mongoose from "mongoose";
 export const initMongoConnection = async () => {
   try {
     const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGODB_PASSWORD;
+
+    console.log("🚀 ~ initMongoConnection ~ user:", user);
+
+    const pwd = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+    console.log("🚀 ~ initMongoConnection ~ pwd:", pwd);
+
     const url = process.env.MONGODB_URL;
+
+    console.log("🚀 ~ initMongoConnection ~ url:", url);
+
     const db = process.env.MONGODB_DB;
 
-    const mongoDBURL = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+    console.log("🚀 ~ initMongoConnection ~ db:", db);
 
+    const mongoDBURL = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0&tls=true`;
+
+    console.log("🚀 ~ initMongoConnection ~ mongoDBURL:", mongoDBURL);
     await mongoose.connect(mongoDBURL);
     console.log("Mongo connection successfully established!");
   } catch (error) {
